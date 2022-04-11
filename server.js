fcb9c10755b6c5b5031737c7789af7638a87c190
Juliana Cristina da Silva <76570640+juliana-silva-hub@users.noreply.github.com>
2022-04-11 17:38:57 -0300

    Update server.js

--- server.js
+++ server.js
@@ -12,5 +12,9 @@ server.get("/", function(req, res) {
     return res.sendFile(__dirname + "/index.html")
 })
 
+
+server.get("/ideias", function(req, res) {
+    return res.sendFile(__dirname + "/ideias.html")
+})
 // liguei meu servidor na porta 3000
 server.listen(3000)

48d7388e7fd33759b49e365e173d93bb70a43e3d
Juliana Cristina da Silva <76570640+juliana-silva-hub@users.noreply.github.com>
2022-04-28 14:46:37 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,43 @@
 const express = require("express")
 const server = express()
 
+
+const ideas = [
+    {
+        img: "/img/worker.png",
+        title: "Cursos de Programação",
+        category: "Estudo",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi sit quia voluptatem",
+        url: "https://rocketseat.com.br"
+    },
+
+    {
+        img: "/img/treino.png",
+        title: "Exercícios",
+        category: "Saúde",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi sit quia voluptatem",
+        url: "https://www.queimadiaria.com/"
+    },
+    
+    {
+        img: "/img/meditacao.png",
+        title: "Meditação",
+        category: "Mentalidade",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi sit quia voluptatem",
+        url: "https://prileiteyoga.com.br/yoga-co/"
+    },
+
+    {
+        img: "/img/karaoke.png",
+        title: "Karaokê",
+        category: "Diversão em família",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi sit quia voluptatem",
+        url: "https://www.singsnap.com/"
+    },
+
+]
+
+
 // configurar arquivos estáticos (css, scripts, imagens)
 server.use(express.static("public"))
 
@@ -9,6 +46,7 @@ server.use(express.static("public"))
 const nunjucks = require("nunjucks")
 nunjucks.configure("views", {
     express: server,
+    noCache: true, //boolean
 })
 
 // criei uma rota/

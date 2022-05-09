18d4d565ab2974b390240388ee1a817e6f42a7e6
Juliana Cristina da Silva <76570640+juliana-silva-hub@users.noreply.github.com>
2022-05-09 14:30:56 -0300

    Update db.js

--- db.js
+++ db.js
@@ -3,31 +3,62 @@ const db = new sqlite3.Database('./potencia_tech_and_rocketseat.db')
 
 db.serialize(function() {
 
-    //criar a tabela
+    //Criar a tabela
     db.run(`
-    CREATE TABLE IF NOT EXISTS ideas(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        image TEXT,
-        title TEXT,
-        category TEXT,
-        description TEXT,
-        link TEXT
-    );
+        CREATE TABLE IF NOT EXISTS ideas(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT,
+            title TEXT,
+            category TEXT,
+            description TEXT,
+            link TEXT
+        );
     `)
-    //inserir dados na tabela
-    const query = `
-    INSERT INTO ideas(
-        image,
-        title,
-        category,
-        description,
-        link
-    ) VALUES (?, ?, ?, ?, ?); 
-    `
-    db.run(query, [
-        
-    ])
-    //consultar dados na tabela
 
-    //deletar um dado da tabela
+    //Inserir dados na tabela
+  //  const query = `
+  //      INSERT INTO ideas(
+  //          image,
+  //          title,
+  //          category,
+  //          description,
+  //          link
+  //  ) VALUES (?, ?, ?, ?, ?); 
+ //   `
+    
+  //  const values = [
+   //     "../public/img/worker.png",
+  //      "Cursos de Programação",
+  //      "Estudo",
+  //      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi sit quia voluptatem",
+  //      "https://rocketseat.com.br"
+  //  ]
+
+
+//    db.run(query, values, function(err) {
+ //       if(err) return console.log(err)
+
+ //      console.log(this)
+ //   })
+
+
+    //Deletar um dado da tabela
+    //db.run(`DELETE FROM ideas WHERE id = ?`, [3], function(err) {
+    //   if(err) return console.log(err)
+
+    //    console.log("DELETEI", this)
+   // })
+
+
+
+    //Consultar dados na tabela
+ //   db.all(`SELECT * FROM ideas`, function(err, rows) {
+ //       if(err) return console.log(err)
+
+ //       console.log(rows)
+ //   })
+   
 })
+
+
+module.exports = db

cb7596a717f613f54b023a555eb8457fdce49889
Juliana Cristina da Silva <76570640+juliana-silva-hub@users.noreply.github.com>
2022-05-18 16:53:17 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,40 +4,45 @@ const server = express()
 
 const db = require("./db")
 
-//const ideas = [
-//    {
-//        
-//    },
-//
-//    {
-//       img: "/img/treino.png",
-//        title: "Exercícios",
-//       category: "Saúde",
-//       description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi sit quia voluptatem",
-//        url: "https://www.queimadiaria.com/"
-//    },
-//   
-//   {
-//       img: "/img/meditacao.png",
-//       title: "Meditação",
-//       category: "Mentalidade",
-//      description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi sit quia voluptatem",
-//       url: "https://prileiteyoga.com.br/yoga-co/"
-//   },
+//const ideas = 
+[{
 
-//   {
-//       img: "/img/karaoke.png",
-//       title: "Karaokê",
-//       category: "Diversão em família",
-//       description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi sit quia voluptatem",
-//       url: "https://www.singsnap.com/"
-//   },
-//
-//]
+    },
+
+    {
+        img: "/img/treino.png",
+        title: "Exercícios",
+        category: "Saúde",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi sit quia voluptatem",
+        url: "https://www.queimadiaria.com/"
+    },
+
+    {
+        img: "/img/meditacao.png",
+        title: "Meditação",
+        category: "Mentalidade",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi sit quia voluptatem",
+        url: "https://prileiteyoga.com.br/yoga-co/"
+    },
+
+    {
+        img: "/img/karaoke.png",
+        title: "Karaokê",
+        category: "Diversão em família",
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi sit quia voluptatem",
+        url: "https://www.singsnap.com/"
+    },
+
+]
 
 
 // configurar arquivos estáticos (css, scripts, imagens)
 server.use(express.static("public"))
+
+// habiitar uso do req.body
+server.use(express.urlencoded({
+    extended: true
+}))
 
 // configuração do nunjucks
 const nunjucks = require("nunjucks")
@@ -51,7 +56,10 @@ nunjucks.configure("views", {
 server.get("/", function (req, res) {
 
     db.all(`SELECT * FROM ideas`, function (err, rows) {
-        if (err) return console.log(err)
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados!")
+        }
 
         const reversedIdeas = [...rows].reverse()
 
@@ -73,14 +81,43 @@ server.get("/", function (req, res) {
 server.get("/ideias", function (req, res) {
 
     db.all(`SELECT * FROM ideas`, function (err, rows) {
-        if (err) return console.log(err)
+        if (err) {
+            console.log(err)
+            return res.send("Erro no banco de dados!")
+        }
 
-        const reversedIdeas = [...rows].reverse()
-        return res.render("../views/ideias.html", { ideas: reversedIdeas() })
+        const reversedIdeas
+        return res.send("OK")[...rows].reverse()
+        return res.render("../views/ideias.html", {
+            ideas: reversedIdeas()
+        })
 
-    })    
+    })
 })
 
+server.post("/", function (req, res) {
+    // Inserir dados na tabela
+    const query = `
+INSERT INTO ideas(
+     image,
+     title,
+     category,
+     description,
+    link
+) VALUES (?, ?, ?, ?, ?); 
+`
+
+    const values = [
+        
+    ]
+
+
+    db.run(query, values, function (err) {
+        if (err) return console.log(err)
+
+        console.log(this)
+    })
+})
 
 // liguei meu servi]dor na porta 3000
 server.listen(3000)
